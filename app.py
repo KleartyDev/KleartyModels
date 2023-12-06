64e7d731ba599432fcf4afcc02ab1546038f2201
Diego Galvan <diego.r.galvan@gmail.com>
2023-12-06 17:42:07 -0300

    Adecuación para poder ejecutar correctamente

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ if server_name and ':' in server_name:
 else:
     port = 5000
     host = "localhost"
-app.run(host=host, port=port)
+#app.run(host=host, port=port)
 
 # Supongamos que esta es tu lista de depósitos inicial
 depositos = ["DEP01", "DEP02", "DEP03", "DEP04"]
@@ -19,15 +19,17 @@ depositos = ["DEP01", "DEP02", "DEP03", "DEP04"]
 @app.route('/')
 def index():
     return render_template('index.html', depositos=depositos)
+    #return 'Web App with Python Flask!'
 
 @app.route('/confirmar', methods=['POST'])
 def confirmar():
-#    # Aquí manejarías los depósitos seleccionados que se reciben del frontend
+    # Aquí manejarías los depósitos seleccionados que se reciben del frontend
     depositos_seleccionados = request.form.getlist('depositos_agregados[]')
     print("depositos seleccionados: ",depositos_seleccionados)
-#    # Hacer algo con los depósitos seleccionados
+    # Hacer algo con los depósitos seleccionados
     #return 'Depósitos confirmados!'
     return (str(depositos_seleccionados))
 
 if __name__ == '__main__':
     app.run(debug=True)
+#app.run(host=host, port=port)
