83ed364c41d6ff307e9ee45fec44b4c6c745859f
giulianogg <giuliano.guaragna@gmail.com>
2024-02-13 20:41:18 -0300

    arreglo puerto

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def realizar_predeciccion():
      
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
 #if __name__ == '__main__':
     ##app.run(debug=True)
